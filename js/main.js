722ccddc3c978a9bd12567e8f86dd6ff89bc4a79
ivatsu <ivan.tsud@gmail.com>
2018-01-10 15:26:44 +0200

    init project structure

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.button');
+
+button.addEventListener('click', function() {
+    console.log('yay, the button is clicked');
+    button.classList.add('red');
+});
+
+// TODO: A modal window with animation when click on button, the window show up.
